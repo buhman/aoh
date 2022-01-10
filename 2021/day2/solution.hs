25389832febc1e30ad46c5523c9f772b30007abb
Zack Buhman <zack@buhman.org>
2022-01-10 00:40:57 -0800

    add day2 part2

--- 2021/day2/solution.hs
+++ 2021/day2/solution.hs
@@ -33,8 +33,25 @@ solve state (instruction:remaining) = solve nextState remaining
   where
     nextState = step state instruction
 
+part1 :: [Instruction] -> Int
+part1 instructions = position state * depth state
+  where
+    state = solve (State 0 0 0) instructions
+
+step2 :: State -> Instruction -> State
+step2 state (Down n) = state { aim = (aim state) + n }
+step2 state (Up n) = state { aim = (aim state) - n }
+step2 state (Forward n) = state { position = position', depth = depth' }
+  where position' = (position state) + n
+        depth' = (depth state) + (aim state) * n
+
+part2 :: [Instruction] -> Int
+part2 = answer . foldl step2 (State 0 0 0)
+  where
+    answer state = position state * depth state
+
 main :: IO ()
 main = do
   instructions <- parseInput <$> readFile "input.txt"
-  let part1 = solve (State 0 0 0) instructions
-  print $ (position part1 * depth part1)
+  putStrLn $ "part1 " <> (show $ part1 instructions)
+  putStrLn $ "part2 " <> (show $ part2 instructions)

28b2b1fe8eaae760a5ef62f99ad1063cdd55e90f
Zack Buhman <zack@buhman.org>
2022-01-11 01:47:22 -0800

    day11 part2

--- 2020/day11/solution.hs
+++ 2020/day11/solution.hs
@@ -1,5 +1,4 @@
 import qualified Data.Vector as V
-import Debug.Trace (traceShowId)
 
 data Grid a = Grid
   { gridVector :: V.Vector a
@@ -8,13 +7,18 @@ data Grid a = Grid
   }
 
 data Point = Point Int Int
-  deriving Eq
+  deriving (Eq, Show)
 
 (!) :: Grid a -> Point -> a
 g ! p = gridVector g V.! asIndex g p
 
 asIndex :: Grid a -> Point -> Int
 asIndex g (Point x y) = y * gridWidth g + x
+
+fromIndex :: Grid a -> Int -> Point
+fromIndex g i = Point x y
+  where
+    (y, x) = divMod i (gridWidth g)
 
 adjacent :: Point -> [Point]
 adjacent (Point x y)
@@ -62,35 +66,82 @@ instance Show a => Show (Grid a) where
                    | y <- [0 .. gridHeight g - 1]
                    ]
 
-rule1 :: Grid Seat -> Point -> Seat -> Seat
-rule1 g p seat = case seat of
-              Empty    -> if occupied == 0 then Occupied else seat
-              Occupied -> if occupied >= 4 then Empty    else seat
-              _        -> seat
+rule1 :: Grid Seat -> Int -> Seat -> Seat
+rule1 g i seat = case seat of
+                   Empty    -> if occupied == 0 then Occupied else seat
+                   Occupied -> if occupied >= 4 then Empty    else seat
+                   _        -> seat
   where
     occupied = length
              . filter ((== Occupied) . (g !))
              . filter (pointInGrid g)
              . adjacent
-             $ p
+             $ fromIndex g i
 
-rule1i :: Grid Seat -> Int -> Seat -> Seat
-rule1i g i = rule1 g (Point x y)
-  where (y, x) = divMod i (gridWidth g)
+type Rule = Grid Seat -> Int -> Seat -> Seat
 
-step1 :: Grid Seat -> Grid Seat
-step1 g = g { gridVector = V.imap (rule1i g) (gridVector g) }
+step :: Rule -> Grid Seat -> Grid Seat
+step rule g = g { gridVector = V.imap (rule g) (gridVector g) }
 
-part1 :: Grid Seat -> Int
-part1 last = let next = step1 last
-             in if (gridVector next) == (gridVector last)
-                then occupied $ gridVector next
-                else part1 next
+solve :: Rule -> Grid Seat -> Int
+solve rule last = let next = step rule last
+                  in if (gridVector next) == (gridVector last)
+                     then occupied $ gridVector next
+                     else solve rule next
   where
     occupied = length . V.filter (== Occupied)
 
+first :: [a] -> [a]
+first (x:_) = [x]
+first [] = []
+
+castRays :: (Seat -> Bool) -> Grid Seat -> Point -> [Point]
+castRays pred g p = points
+  where
+    rays :: [ ((Int -> Int), (Int -> Int)) ]
+    rays = let s = subtract
+           in [ ((s 1), (s 1))
+              , ((+ 0), (s 1))
+              , ((+ 1), (s 1))
+              , ((s 1), (+ 0))
+
+              , ((+ 1), (+ 0))
+              , ((s 1), (+ 1))
+              , ((+ 0), (+ 1))
+              , ((+ 1), (+ 1))
+              ]
+
+    addRay (Point x y) (fx, fy) = Point (fx x) (fy y)
+
+    rayPoints point ray = let p = (addRay point ray)
+                          in p:(rayPoints p ray)
+
+    points = [ pt
+             | ray <- rays
+             , pt <- first . filter (pred . (g !)) . takeWhileOne ((==Floor) . (g !))
+                                                   . takeWhile (pointInGrid g)
+                                                   . rayPoints p
+                                                   $ ray
+             ]
+
+takeWhileOne :: (a -> Bool) -> [a] -> [a]
+takeWhileOne pred [] = []
+takeWhileOne pred (x:xs) = if pred x then x : takeWhileOne pred xs else [x]
+
+rule2 :: Grid Seat -> Int -> Seat -> Seat
+rule2 g i seat = case seat of
+                   Empty    -> if occupied == 0 then Occupied else seat
+                   Occupied -> if occupied >= 5 then Empty    else seat
+                   _        -> seat
+  where
+    occupied = length
+             . castRays (==Occupied) g
+             $ fromIndex g i
 
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "input.txt"
+  let part1 = solve rule1
+      part2 = solve rule2
   putStrLn $ "part1 " <> (show $ part1 input)
+  putStrLn $ "part2 " <> (show $ part2 input)
